25af8ba63bcb4f6b27401d77e4d4c3c5658dabb6
andres30xed <andres.esquivel@tresastronautas.com>
2021-11-05 12:06:29 -0500

    limpiando proyecto

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,18 +1,11 @@
 import { Module, HttpModule, Provider } from '@nestjs/common';
-import { EmailService } from './services/email.service';
-import { SMSService } from './services/sms.service';
-
 @Module({
   imports: [
     HttpModule,
   ],
   providers: [
-    EmailService,
-    SMSService,
   ],
   exports: [
-    EmailService,
-    SMSService,
   ],
 })
 export class SharedModule {
